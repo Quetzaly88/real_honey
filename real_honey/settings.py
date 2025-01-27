a7685ac8b080b0923c646c2a7bc1c2328d1df05b
Andrea Nilsson <andreaycoral@icloud.com>
2025-01-27 17:14:48 +0000

    Install pillow dependency. Add static files setup with CSS directory. Update base.html

--- real_honey/settings.py
+++ real_honey/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -148,9 +149,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' #Url to access static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), #tell Django where to find static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
